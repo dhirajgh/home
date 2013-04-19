599a754155aedec7629de94d1a9624462b582133
Dhiraj <dhirajshrivastav@outlook.com>
2013-04-19 23:46:28 +0530

    commiting in test dot php after checking out branch

--- test.php
+++ test.php
@@ -3,7 +3,9 @@
     // This file is a php test file.
     //Checking for branching and git features for working on PHP development environment
     
-    echo "We are into the test dot php file"
+    echo "We are into the test dot php file";
     
     //Now I am modifying in the home2 branch.
+    echo"<br/>";
+    echo "Only changing in test dot php";
 ?>
